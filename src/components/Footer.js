2d8f5c927060b4c19d38dc3e237960431a11fba3
mamismeda <amamismeda@gmail.com>
2023-02-16 20:33:29 +0400

    created slider

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Fade } from 'react-awesome-reveal';
+
+const Footer = () => {
+  return (
+    <footer className="bck_red">
+      <Fade triggerOnce delay={500}>
+        <div className="font_righteous footer_logo_venue">The venue</div>
+        <div className="footer_copyright">The venue 2023 all rights reserved</div>
+      </Fade>
+    </footer>
+  );
+};
+
+export default Footer;
