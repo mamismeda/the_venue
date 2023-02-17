becc2a5d921d6b4382a97e3d6ef1b85fcd207542
mamismeda <amamismeda@gmail.com>
2023-02-17 19:18:28 +0400

    added another section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./resources/styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Featured from './components/featured';
+import VenueNfo from "./components/venueNfo";
 
 const App = () => {
   return (
@@ -12,6 +13,9 @@ const App = () => {
 
       <>
        <Featured />
+      </>
+      <>
+       <VenueNfo />
       </>
 
       <div style={{backgroundColor: 'red', height:'800px'}}></div>
